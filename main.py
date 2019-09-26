d2bae94a41b3e511d9c53b8b3bd98382fdee9cf4
eluxo <eluxo@risky.ubuntu>
2019-09-26 15:54:04 +0200

    main: implement message handler

--- main.py
+++ main.py
@@ -3,17 +3,51 @@
 
 import csv
 import messages
+import database
 import utils
 import traceback as tr
+
+from messages import MESSAGE_TYPE as MessageType
 
 #LOGFILE='/mnt/c/Users/nexus/Documents/Elder Scrolls Online/live/Logs/encounterlog.backup.txt'
 LOGFILE='/mnt/c/Users/nexus/Documents/Elder Scrolls Online/live/Logs/Encounter.log'
 #LOGFILE="samples/sample1.log"
 
-def handler(source, message):
-    print "%4i %s" % (source.getLines(), message.type)
+class Handler:
+    def __init__(self):
+        self.startTime = None
+        self.endTime = None
+        self.unitRegistry = database.UnitRegistry()
+        self.abilityRegistry = database.AbilityRegistry()
 
+    def handle(self, source, message):
+        self.unitRegistry.handle(message)
+        self.abilityRegistry.handle(message)
+
+        if message.typeId == MessageType.BEGIN_COMBAT:
+            self.startTime = message.time
+            
+        elif message.typeId == MessageType.END_COMBAT:
+            self.endTime = message.time
+            self.calculateStats()
+            
+        elif message.typeId == MessageType.COMBAT_EVENT:
+            self.combatEvent(message)
+    
+    def calculateStats(self):
+        duration = self.endTime - self.startTime
+        h = duration / (60 * 60 * 1000)
+        m = (duration / (60 * 1000)) % 60
+        s = (duration / 1000) % 60
+        ms = duration % 1000
+        print "total time in encounter: [%02d:%02d:%02d.%03d]" % (h, m, s, ms)
+    
+    def combatEvent(self, message):
+        pass
+        
+
+handler = Handler()
 utils.EventSource.createFileReaderSource(LOGFILE) \
-    .setObserver(handler) \
+    .setObserver(handler.handle) \
     .loop()
 
