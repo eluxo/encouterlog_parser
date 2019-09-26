1d2cb79f89d89d00beae3e553e8a2ab356f8f359
eluxo <eluxo@risky.ubuntu>
2019-09-26 14:23:41 +0200

    main: change to use EventSource implementation

--- main.py
+++ main.py
@@ -3,31 +3,17 @@
 
 import csv
 import messages
+import utils
 import traceback as tr
 
 #LOGFILE='/mnt/c/Users/nexus/Documents/Elder Scrolls Online/live/Logs/encounterlog.backup.txt'
-#LOGFILE='/mnt/c/Users/nexus/Documents/Elder Scrolls Online/live/Logs/Encounter.log'
-LOGFILE="samples/sample1.log"
-fp = open(LOGFILE, "rb")
-reader = csv.reader(fp)
+LOGFILE='/mnt/c/Users/nexus/Documents/Elder Scrolls Online/live/Logs/Encounter.log'
+#LOGFILE="samples/sample1.log"
 
-factory = messages.MessageFactory()
+def handler(source, message):
+    print "%4i %s" % (source.getLines(), message.type)
 
-loglines = []
-types = {}
-i = 0
-for row in reader:
-    try:
-        i += 1
-        message = factory.create(row)
-        loglines.append(message)
-        types[message.type] = 0
-    except:
-        print i, row
-        tr.print_exc()
-        break
-
-for type in types.keys():
-    print "\t'%s': Message," % (type)
-
+utils.EventSource.createFileReaderSource(LOGFILE) \
+    .setObserver(handler) \
+    .loop()
 
