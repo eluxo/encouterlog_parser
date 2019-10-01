be75f6f61423cbf1e424d684bdf8c3c4852c7478
eluxo <eluxo@risky.ubuntu>
2019-10-01 19:07:34 +0200

    utils: fix time import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-import time
 import csv
 import threading
 
@@ -12,6 +11,8 @@ import traceback as tr
 from messages import MessageFactory
 
 from utils.time import GameClock
+
+import time
 
 class TailSource:
     '''
